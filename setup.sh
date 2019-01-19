a60663c426a612f9eafc1fb807380a16ff05edfd
Arkadiy Garber <arkadiygarber@Arkadiys-MacBook-Pro.local>
2019-01-19 14:18:31 -0700

    modified setup.sh and test_run files

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ NC='\033[0m'
 
 gzip -d HMM-lib.tar.gz
 tar xf HMM-lib.tar
+rm HMM-lib.tar
 
 gzip -d test_dataset.tar.gz
 tar xf test_dataset.tar
+rm test_dataset.tar
 
 printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
