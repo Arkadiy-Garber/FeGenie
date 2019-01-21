75604096f59b6edf89596424dd9c4d9456643000
Arkadiy-Garber <arkg@udel.edu>
2019-01-20 23:26:49 -0700

    Update test_run_regular.sh

--- test_run_regular.sh
+++ test_run_regular.sh
@@ -1,1 +1,1 @@
-FeGenie.py -bin_dir test_dataset -bin_ext txt -out fegenie_out -hmm_lib HMM-lib -r rscripts
+FeGenie.py -bin_dir test_dataset -bin_ext txt -out fegenie_out -hmm_lib HMM-lib -R rscripts
