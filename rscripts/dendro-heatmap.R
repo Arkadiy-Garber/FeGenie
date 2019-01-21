ff681c722d0e96996ddbf3f2748d4e29efa6430b
Arkadiy-Garber <arkg@udel.edu>
2019-01-20 23:17:58 -0700

    Add files via upload

---
+++ rscripts/dendro-heatmap.R
@@ -0,0 +1,67 @@
+library.path <- .libPaths()
+library("argparse", lib.loc = library.path)
+library("ggdendro", lib.loc = library.path)
+library("reshape2", lib.loc = library.path)
+library("grid", lib.loc = library.path)
+library("ggplot2", lib.loc = library.path)
+
+#library("ggplot2")
+#library("ggdendro")
+#library("reshape2")
+#library("grid")
+#library("argparse")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+fegenie <- read.csv(args[1])
+l = length(fegenie)
+fegenie.sub <- t(fegenie[,3:l-1])
+
+fegenie.matrix <- as.matrix(fegenie.sub)
+
+colnames(fegenie.matrix) <- as.vector(fegenie$X)
+
+
+fegenie.scaled = fegenie.matrix
+fegenie.scaled <- scale(fegenie.scaled)
+
+fegenie.dendro <- as.dendrogram(hclust(d = dist(x = fegenie.scaled)))
+dendro.plot <- ggdendrogram(data = fegenie.dendro, rotate = TRUE)
+
+outfile = paste(args[2], "Fegenie-dendro.tiff", sep = "/", collapse = NULL)
+tiff(outfile, units="in", width=12, height=10, res=300)
+dendro.plot
+dev.off()
+
+#********************************************************************************
+#***************************** DENDROGRAM *********************************
+#********************************************************************************
+fegenie.long <- melt(fegenie.scaled, id = c(X1))
+fegenie.order <- order.dendrogram(fegenie.dendro)
+fegenie.long$Var1 <- factor(x = fegenie.long$Var1,
+                          levels = fegenie.long$Var1[fegenie.order], 
+                          ordered = TRUE)
+
+
+
+heatmap.plot <- ggplot(data = fegenie.long, aes(x = Var2, y = Var1)) +
+  geom_tile(aes(fill = value)) +
+  scale_fill_gradient2() +
+  theme(axis.text.y = element_text(size = 10))
+
+
+# REPOSITION LEGEND
+heatmap.plot <- ggplot(data = fegenie.long, aes(x = Var2, y = Var1)) +
+  geom_tile(aes(fill = value)) +
+  scale_fill_gradient2() +
+  theme(axis.text.y = element_text(size = 10), axis.text.x = element_text(size = 8),
+        legend.position = "top") + theme(axis.text.x = element_text(vjust = 1, angle = 45, color = "black", size = 12, hjust=1))
+
+outfile = paste(args[2], "Fegenie-heatmap.tiff", sep = "/", collapse = NULL)
+tiff(outfile, units="in", width=12, height=10, res=300)
+heatmap.plot
+dev.off()
+
+
+
+
