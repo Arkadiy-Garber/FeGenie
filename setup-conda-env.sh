3c06f6db7f04cf065e55e0ffa6f190fdcaad026c
Arkadiy-Garber <arkg@udel.edu>
2021-11-28 16:56:35 -0500

    Add files via upload

---
+++ setup-conda-env.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# setting colors to use
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+gzip -d HMM-lib.tar.gz
+tar xf HMM-lib.tar
+rm HMM-lib.tar
+
+gzip -d test_dataset.tar.gz
+tar xf test_dataset.tar
+rm test_dataset.tar
+
+printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
+
+## adding conda channels
+conda config --add channels defaults 2> /dev/null
+conda config --add channels bioconda 2> /dev/null
+conda config --add channels conda-forge 2> /dev/null
+conda config --add channels au-eoed 2> /dev/null
+
+## creating GToTree environment and installing dependencies
+conda create -n fegenie hmmer diamond prodigal blast --yes
+
+## activating environment
+source activate fegenie
+
+## creating directory for conda-env-specific source files
+mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
+
+## adding FeGenie bin path and GToTree_HMM_dir variable:
+echo '#!/bin/sh'" \
+
+export PATH=\"$(pwd):"'$PATH'\"" \
+
+export rscripts=\"$(pwd)/rscripts\"
+
+export HMM_dir=\"$(pwd)/HMM-lib\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
+
+# re-activating environment so variable and PATH changes take effect
+source activate fegenie
+
+
+printf "\n        ${GREEN}DONE!${NC}\n\n"
